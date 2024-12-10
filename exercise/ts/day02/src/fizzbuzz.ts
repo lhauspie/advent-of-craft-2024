5b26c223b6b58421967254781053c6e11a89b809
Logan HAUSPIE <logan.hauspie.pro@gmail.com>
2024-12-10 10:00:48 +0100

    :tada: Day 02

--- exercise/ts/day02/src/fizzbuzz.ts
+++ exercise/ts/day02/src/fizzbuzz.ts
@@ -3,25 +3,31 @@ import {none, Option, some} from "fp-ts/Option";
 export const min = 1;
 export const max = 100;
 
-let mapping: Map<number, string> = new Map([
-    [15, 'FizzBuzz'],
+let default_mapping: Map<number, string> = new Map([
     [3, 'Fizz'],
     [5, 'Buzz'],
 ]);
 
-export const fizzbuzz = (input: number): Option<string> =>
-    isOutOfRange(input)
-        ? none
-        : some(convertSafely(input));
-
-function convertSafely(input: number): string {
+function convertSafely(mapping: Map<number, string>, input: number): string {
+    let values: string[] = [];
     for (const [divisor, value] of mapping) {
         if (is(divisor, input)) {
-            return value;
+            values.push(value);
         }
+    }
+    if (values.length > 0) {
+        return values.join('');
     }
     return input.toString();
 }
 
 const is = (divisor: number, input: number): boolean => input % divisor === 0;
 const isOutOfRange = (input: number): boolean => input < min || input > max;
+
+
+export const fizzbuzzFactory = (mapping: Map<number, string> = default_mapping) => {
+    return (input: number): Option<string> =>
+        isOutOfRange(input)
+            ? none
+            : some(convertSafely(mapping, input));
+}
--- exercise/ts/day02/tests/fizzbuzz.spec.ts
+++ exercise/ts/day02/tests/fizzbuzz.spec.ts
@@ -1,20 +1,30 @@
-import {fizzbuzz, max, min} from '../src/fizzbuzz';
+import {fizzbuzzFactory, max, min} from '../src/fizzbuzz';
 import * as O from 'fp-ts/Option';
 import {isNone, isSome} from 'fp-ts/Option';
 import * as fc from 'fast-check';
 import {pipe} from 'fp-ts/function';
 
 describe('FizzBuzz should return', () => {
+    const fizzbuzz = fizzbuzzFactory(new Map([
+        [3, 'Fizz'],
+        [5, 'Buzz'],
+        [7, 'Whizz'],
+        [11, 'Bang'],
+    ]));
+
     test.each([
         [1, '1'],
         [67, '67'],
         [82, '82'],
         [3, 'Fizz'],
-        [66, 'Fizz'],
-        [99, 'Fizz'],
+        [66, 'FizzBang'],
+        [99, 'FizzBang'],
         [5, 'Buzz'],
         [50, 'Buzz'],
         [85, 'Buzz'],
+        [7, 'Whizz'],
+        [77, 'WhizzBang'],
+        [35, 'BuzzWhizz'],
         [15, 'FizzBuzz'],
         [30, 'FizzBuzz'],
         [45, 'FizzBuzz']
@@ -31,7 +41,10 @@ describe('FizzBuzz should return', () => {
         fc.assert(
             fc.property(
                 fc.integer().filter(n => n >= min && n <= max),
-                (n) => isConvertValid(n)
+                (n) => {
+                    console.log(n, fizzbuzz(n))
+                    return isConvertValid(n);
+                }
             )
         );
     });
@@ -41,7 +54,19 @@ describe('FizzBuzz should return', () => {
         O.exists(result => validStringsFor(input).includes(result))
     );
 
-    const validStringsFor = (x: number): string[] => ['Fizz', 'Buzz', 'FizzBuzz', x.toString()];
+    const validStringsFor = (x: number): string[] => [
+        'Fizz',
+        'FizzBuzz', 'FizzBuzzWhizz', 'FizzBuzzBang', 'FizzBuzzWhizzBang',
+        'FizzWhizz', 'FizzWhizzBang',
+        'FizzBang',
+        'Buzz',
+        'BuzzWhizz', 'BuzzWizzBang',
+        'BuzzBang',
+        'Whizz',
+        'WhizzBang',
+        'Bang',
+        x.toString()
+    ];
 
     test('none for numbers out of range', () => {
         fc.assert(
